6d4bab4b1e1a2b7f25fd1b4a0ff980cb196cf94a
feracios120499 <37274332+feracios120499@users.noreply.github.com>
2018-08-23 23:36:34 +0300

    Initial commit

---
+++ AdoNetDBFirst/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace AdoNetDBFirst
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbFind = new System.Windows.Forms.TextBox();
+            this.btFind = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbFind
+            // 
+            this.tbFind.Location = new System.Drawing.Point(12, 21);
+            this.tbFind.Name = "tbFind";
+            this.tbFind.Size = new System.Drawing.Size(545, 22);
+            this.tbFind.TabIndex = 0;
+            this.tbFind.TextChanged += new System.EventHandler(this.tbFind_TextChanged);
+            // 
+            // btFind
+            // 
+            this.btFind.Enabled = false;
+            this.btFind.Location = new System.Drawing.Point(12, 49);
+            this.btFind.Name = "btFind";
+            this.btFind.Size = new System.Drawing.Size(545, 55);
+            this.btFind.TabIndex = 1;
+            this.btFind.Text = "Найти";
+            this.btFind.UseVisualStyleBackColor = true;
+            this.btFind.Click += new System.EventHandler(this.btFind_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 142);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(545, 292);
+            this.listBox1.TabIndex = 2;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(579, 142);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(450, 292);
+            this.listBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Книги";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(576, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Авторы";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1160, 467);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btFind);
+            this.Controls.Add(this.tbFind);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbFind;
+        private System.Windows.Forms.Button btFind;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ AdoNetDBFirst/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdoNetDBFirst
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            using(var db=new LibraryDZ2Entities())
+            {
+                listBox2.Items.AddRange(db.Authors.ToArray());
+            }
+        }
+
+        private void tbFind_TextChanged(object sender, EventArgs e)
+        {
+            if (tbFind.Text.Length > 0)
+            {
+                btFind.Enabled = true;
+            }
+            else
+            {
+                btFind.Enabled = false;
+            }
+        }
+
+        private void btFind_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            using (var db=new LibraryDZ2Entities())
+            {
+
+                foreach(var books in db.Authors.Where(p => p.Name.Contains(tbFind.Text)).ToList().Select(p=>p.Books))
+                {
+                    listBox1.Items.AddRange(books.ToArray());
+                }
+            }
+        }
+    }
+}
